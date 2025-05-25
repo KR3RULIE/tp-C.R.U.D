a6deefa73252b72d9ba78a26619ec32f7cd9ccf1
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-05-25 19:03:04 -0300

    cree un usuario y lo agregue al array de cuentas, y liimpio el formulario

--- js/app.js
+++ js/app.js
@@ -12,13 +12,29 @@ const crearUsuario = () => {
   //TODO: tomar los datos del formulario
 
   // con los datos voy a crear un objeto usuario
-  const usuarioNuevo = new Cuentas();
+  const usuarioNuevo = new Cuentas(
+    inputIDCuenta.value,
+    inputContraseña.value,
+    inputReContraseña.value,
+    inputEmail.value
+  );
   // guardar el usuario en un array
+  cuentas.push(usuarioNuevo);
+  // limpiar el form
+  limpiarFormulario();
+};
+
+const limpiarFormulario = () => {
+  formularioRegistro.reset();
 };
 
 // Variables
 const btnRegistro = document.getElementById("btnRegistro");
 const formularioRegistro = document.querySelector("form");
+const inputIDCuenta = document.querySelector("#idcuenta");
+const inputContraseña = document.querySelector("#contraseña");
+const inputReContraseña = document.querySelector("#recontraseña");
+const inputEmail = document.querySelector("#email");
 const cuentas = [];
 
 // Manejador de eventos
